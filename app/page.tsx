6c84f0cb6b2026745dc09b45f8d8deb945c83644
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-07-19 13:14:57 -0400

    Contain attribution component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Card from "./components/card"
+import AttributionComponent from "./components/card/components/attribution"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 relative">
-      <Card />
+      <div>
+        <Card />
+        <AttributionComponent />
+      </div>
     </main>
   )
 }
