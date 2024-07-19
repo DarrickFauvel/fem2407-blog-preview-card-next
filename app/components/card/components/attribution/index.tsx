a078fe00e73ca263f176f04adf08eb5a73b4dc45
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-07-19 13:15:09 -0400

    Create attribution component

---
+++ app/components/card/components/attribution/index.tsx
@@ -0,0 +1,19 @@
+const AttributionComponent = () => {
+  return (
+    <div className="text-[11px] text-gray-600 text-center mt-3">
+      Challenge by{" "}
+      <a
+        className="text-gray-700 underline"
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a className="text-gray-700 underline" href="#">
+        Darrick Fauvel
+      </a>
+      .
+    </div>
+  )
+}
+export default AttributionComponent
