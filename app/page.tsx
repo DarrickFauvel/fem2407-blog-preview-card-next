3fcf3969e0c3075e4c88c029551f67163ea324d8
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-07-20 06:11:31 -0400

    Remove center justification on desktop view

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import AttributionComponent from "./components/card/components/attribution"
 
 export default function Home() {
   return (
-    <main className="relative flex flex-col justify-center items-center min-h-screen">
+    <main className="relative flex flex-col sm:justify-center items-center min-h-screen">
       <Card />
       <AttributionComponent />
     </main>
