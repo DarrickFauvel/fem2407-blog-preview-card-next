4b405394ad1e93a231465706a8bbd5b571212fcf
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-07-20 05:52:31 -0400

    Adjust flex

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,9 @@ import AttributionComponent from "./components/card/components/attribution"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 relative">
-      <div>
-        <Card />
-        <AttributionComponent />
-      </div>
+    <main className="relative flex flex-col justify-center items-center min-h-screen">
+      <Card />
+      <AttributionComponent />
     </main>
   )
 }
